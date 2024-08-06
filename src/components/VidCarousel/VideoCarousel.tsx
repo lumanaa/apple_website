f95ba2f207024b865d340aa4188fceb327f165e4
lumanaa <lssuwal@gmail.com>
2024-08-07 01:07:38 +0545

    carousel first push

--- src/components/VidCarousel/VideoCarousel.tsx
+++ src/components/VidCarousel/VideoCarousel.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import { hightlightsSlides } from "../../constants";
+import "./vidCarousel.scss";
 
 const VideoCarousel = () => {
   return (
-    <div>VideoCarousel</div>
-  )
-}
+    <>
+      <div className='carousel-section'>
+        {hightlightsSlides.map((list, i) => (
+          <div key={list.id} id='slider' className='slider'>
+            <div className='container'>
+              <div className='slides'></div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
 
-export default VideoCarousel
+export default VideoCarousel;
